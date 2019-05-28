80249a316b804e88799e81d4085be38a2980ceca
Mario Adamo Jr <marioadamojr@gmail.com>
2019-05-28 22:53:44 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -9,6 +9,13 @@ class Dog
   end
 
   def self.create_table
+    sql = <<-SQL
+      CREATE_TABLE IF NOT EXISTS dogs (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        breed TEXT)
+    SQL
+    DB[:conn].execute(sql)
   end
 
 end
